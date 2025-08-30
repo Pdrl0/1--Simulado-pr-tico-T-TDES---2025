bd3899c775e988b96a39dbc9a6c277ee128b46da
Pdlr0 <pedro_oliveira9@estudante.sesisenai.org.br>
2025-08-30 09:09:41 -0300

    A2

---
+++ database_setup.sql
@@ -0,0 +1,11 @@
+    create database db_escola;  
+    use db_escola;
+
+create table turmas(
+    id_turma int not null primary key auto_increment;
+    numero_alunos int not null;
+    ano_serie int not null
+
+
+)
+
