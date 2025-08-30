eabfe0737a0759458bcf2be13976b7e277e1cf69
Pdlr0 <pedro_oliveira9@estudante.sesisenai.org.br>
2025-08-30 11:07:55 -0300

    sistema

--- Sistema/database_setup.sql
+++ Sistema/database_setup.sql
@@ -6,14 +6,14 @@ create table turmas(
     numero_alunos int not null;
     ano_serie int not null
 
-)
+);
 
 create table professores(
     id_professores int not null auto_increment;
     email varchar(45) not null,
     senha varchar(45) not null
 
-)
+);
 
 create table atividades(
     id_atividades int not null auto_increment;
@@ -23,4 +23,4 @@ create table atividades(
     foreigne key fk_professor references professores(id_professor),
     foreigne key fk_turma references turmas(id_turma)
 
-)
+);
